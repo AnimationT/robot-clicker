a9f20a687ddcd5a37414ff995aefc71a3e3be6e8
AnimationT <86012532+AnimationT@users.noreply.github.com>
2025-11-08 08:15:22 +0000

    added, things?

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-var clicks = 0;
+var compute = 0;
 var clickAm = 1;
-var autoClickAm = 0;
+var roboClickAm = 0;
 var cost1 = 100;
 var cost2 = 100000;
+let updatesPerSecond = 5;
+let autoInterval;
+
+
 function increase() {
-    clicks+=clickAm;
+    compute+=clickAm;
 }
 
 setInterval(() => {
-    if (clicks >= cost1) {
+    if (compute >= cost1) {
         $("#upgrade1").attr("disabled", false);
         $("#upgrade1").css("background-color", "lightblue");
         $("#upgrade1").css("cursor", "pointer");
@@ -17,7 +21,7 @@ setInterval(() => {
         $("#upgrade1").css("background-color", "grey");
     }
 
-    if (clicks >= cost2) {
+    if (compute >= cost2) {
         $("#upgrade2").attr("disabled", false);
         $("#upgrade2").css("background-color", "lightblue");
         $("#upgrade2").css("cursor", "pointer");
@@ -25,17 +29,37 @@ setInterval(() => {
         $("#upgrade2").attr("disabled", true);
         $("#upgrade2").css("background-color", "grey");
     }
-    $("#clickNum").html(clicks);
+    $("#clickNum").html(compute);
     $("#cost1").html(cost1);
     $("#cost2").html(cost2);
 }, 10);
 
-setInterval(() => {
-    clicks+=autoClickAm;
-}, 1000);
+function startAuto() {
+  // clear old interval if already running
+  clearInterval(autoInterval);
+
+  // calculate how much to add per tick
+  let interval = 1000 / updatesPerSecond;
+
+  autoInterval = setInterval(() => {
+    let increment = roboClickAm / updatesPerSecond;
+    compute += increment;
+    document.querySelector(".odometer").innerHTML = Math.floor(compute);
+  }, interval);
+}
+
+// start it
+startAuto();
+
+// whenever roboClickAm changes, just restart to use new value
+function updateRoboClickAm(newAmount) {
+  roboClickAm = newAmount;
+  startAuto(); // restarts with updated speed
+}
+
 
 function inClickAm() {
-    clicks-=cost1;
+    compute-=cost1;
     cost1 = Math.round(cost1*1.25);
     clickAm+=100;
     $("#upgrade1").css("background-color", "blue");
@@ -44,12 +68,28 @@ function inClickAm() {
     }, 10);
 }
 
-function autoClick1() {
-    clicks-=cost2;
+function roboClick1() {
+    compute-=cost2;
     cost2 = Math.round(cost2*1.25);
-    autoClickAm+=100;
+    updateRoboClickAm(roboClickAm + 100);
     $("#upgrade2").css("background-color", "blue");
     setTimeout(() => {
         $("#upgrade2").css("background-color", "lightblue");
     }, 10);
+}
+
+function hide1() {
+    $("#info1").attr("hidden", true);
+}
+
+function show1() {
+    $("#info1").attr("hidden", false);
+}
+
+function hide2() {
+    $("#info2").attr("hidden", true);
+}
+
+function show2() {
+    $("#info2").attr("hidden", false);
 }
